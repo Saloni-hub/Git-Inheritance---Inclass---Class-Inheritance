a6e86fb5fd70c57cadfd6af75879d4fc9c810904
saloni-hub <pandeysaloni4july@gmail.com>
2020-10-29 07:54:12 +0530

    Inheritence code

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
-// import using require
-
+// import using require 
+//es6 default import syntax
+import Shape from './shape';
 // declare class
 
-
 // export class using module.exports
+export default class Circle extends Shape {
+    constructor() {
+        // first line of subclass constructor should call superclass constructor with appropriate parameter (Super)
+        super();
+    }
+    calculateArea() {
+        console.log("Area of circle: "+ this.color);
+        return 3.14*5*5;
+    }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,27 @@
 // declare class
 
 // export class using module.exports
+
+/*Acceptance Criteria
+
+Create a base class called "Shape" with methods drawShape, calculateArea and color property that is initialised with a color.
+
+In another module make a class called "Circle" that extends the "Shape" class and overrides the calculateArea method.
+
+Write code for Shape class in shape.js and Circle in circle.js
+
+Export the class from each file. */
+ 
+export default class Shape {
+    constructor() {
+        this.color = "blue";
+    }
+    drawShape() {
+        console.log("Drawing a shape");
+    }
+    calculateArea() {
+        console.log("Don't Know area of unknown shape");
+        return 0;
+    }
+}
+
